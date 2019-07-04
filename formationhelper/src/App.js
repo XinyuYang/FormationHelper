6120bab421289e4ca7f64deb3c09f09c08f80413
yurenpang <ypang@macalester.edu>
2019-07-04 18:27:13 -0500

    Complete initial layout.

--- formationhelper/src/App.js
+++ formationhelper/src/App.js
@@ -5,6 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <div id="top-bar">
+            <p>This is the top bar</p>
+        </div>
+
+        <div id="side-bars">
+            <div id="left-side-bar" className="box">
+                <p>This is the left bar</p>
+            </div>
+
+            <div id="right-side-bar" className="box">
+                <p>This is the right bar</p>
+            </div>
+        </div>
+
+        <div id="bottom-bar">
+            <p>This is the bottom bar</p>
+        </div>
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
