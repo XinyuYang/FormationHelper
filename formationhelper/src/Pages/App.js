23b106266d974a55fedc14da0f3d7031c5d87bfc
chenyyxx <paranoiark@outlook.com>
2019-07-10 20:36:17 -0400

    Created pages folder and added welcome page

---
+++ formationhelper/src/Pages/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+import HomePage from "./HomePage";
+import MainPage from "./MainPage";
+
+function App() {
+  return (
+      <MainPage/>
+  );
+}
+
+export default App;
