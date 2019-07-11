d8068803bdc4ab02cf65bea4216c54b623a94e02
chenyyxx <paranoiark@outlook.com>
2019-07-11 01:00:25 -0400

    Remove redundant import

--- formationhelper/src/Pages/CreditPage.js
+++ formationhelper/src/Pages/CreditPage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
