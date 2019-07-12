36ef93b6b895588e50a67f6f4d6f4e90f1cbdf30
XinyuYang <xyang2@macalester.edu>
2019-07-12 08:17:26 +0800

    Minor changes

--- formationhelper/src/Pages/MainPage.js
+++ formationhelper/src/Pages/MainPage.js
@@ -1,12 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import SideBar from './Components/SideBar';
 import FormationCanvas from './Components/FormationCanvas';
 import {Card, Grid, Paper} from '@material-ui/core';
 import {makeStyles} from "@material-ui/core";
-=======
 import '../css/MainPage.css';
->>>>>>> d55d8994cb79f1d272f2a1d5ad9342cd43bacd17
 
 const useStyles = makeStyles(theme => ({
     items: {
@@ -19,14 +16,14 @@ function MainPage() {
     const classes = useStyles();
 
     return (
-        <div>
+        <React.Fragment>
             <SideBar/>
             <Grid className={classes.items} container direction="column" justify="center" alignItems="center">
                 <Grid item><Paper>Front</Paper></Grid>
                 <Grid item><FormationCanvas /></Grid>
                 <Grid item marginTop="theme.spacing{4}"><Paper>Back</Paper></Grid>
             </Grid>
-        </div>
+        </React.Fragment>
     );
 }
 
