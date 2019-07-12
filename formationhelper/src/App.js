d5c139e25627286d47947dfec05d0928a578ef5b
chenyyxx <paranoiark@outlook.com>
2019-07-11 23:17:23 -0400

    Remove the duplicate app.js

--- formationhelper/src/App.js
+++ formationhelper/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './css/MainPage.css';
 import WelcomePage from "./Pages/WelcomePage";
 import CreditPage from "./Pages/CreditPage";
+import MainPage from "./Pages/MainPage";
 
 function App() {
   return (
       // Uncomment this to go to WelcomePage
       <React.Fragment>
           <WelcomePage/>
+          <MainPage/>
           <CreditPage/>
       </React.Fragment>
   );
