e0e5baeb3480d169b2da28fe269ac4bad8a3bab4
XinyuYang <xyang2@macalester.edu>
2019-07-17 21:15:17 +0800

    Added functional return buttonn at app header

--- formationhelper/src/Pages/Components/Header.js
+++ formationhelper/src/Pages/Components/Header.js
@@ -5,6 +5,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBack from '@material-ui/icons/ArrowBack';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -26,6 +28,9 @@ export default (headerInfo) => {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
+                    <IconButton edge="start" component={Link} to="/">
+                        <ArrowBack/>
+                    </IconButton>
                     <Typography variant="h6" className={headerInfo.headerTitle}>
                         {headerInfo.headerTitle}
                     </Typography>
