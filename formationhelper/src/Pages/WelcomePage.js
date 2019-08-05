0ed564dd69df6a380dfe17beeccbd490cdddcf5e
XinyuYang <xyang2@macalester.edu>
2019-08-05 17:17:53 +0800

    Wrote Welcome Page in Component  version

--- formationhelper/src/Pages/WelcomePage.js
+++ formationhelper/src/Pages/WelcomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import "../css/WelcomePage.css"
 import "../css/WelcomePage.css"
 import Footer from "./Components/Footer";
@@ -24,79 +24,94 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export default function WelcomePage() {
-    const classes = useStyles();
-    const [dialogOpen, setDialogOpen] = React.useState(false);
+// const [dialogOpen, setDialogOpen] = useState(false);
 
-    function handleClickOpen(){
-        setDialogOpen(true);
+
+class WelcomePage extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            dialogOpen: false
+        }
+        this.handleClickOpen = this.handleClickOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    };
+    
+    handleClickOpen(){
+        this.setState({dialogOpen:true});
     }
 
-    function handleClose(){
-        setDialogOpen(false);
+    handleClose(){
+        this.setState({dialogOpen:false});
     }
+        
+    render(){
 
-    return(
-        <React.Fragment>
-            <Header headerTitle={"Welcome to Formation Helper"}/>
-            <div className="App" >
-                <div>
-                    <h1>Welcome to Formation Helper</h1>
-                </div>
-                <div>
-                    <p>this is a text this is a text this is a text this is a text this is a text</p >
-                    <p>this is a text this is a text this is a text this is a text this is a text</p >
-                    <p>this is a text this is a text this is a text this is a text this is a text</p >
-                    <p>this is a text this is a text this is a text this is a text this is a text</p >
-                    <p>this is a text this is a text this is a text this is a text this is a text</p >
-                </div>
-                < img src="http://hochstein.org/portals/0/gottaSingDance%20pic%20for%20web.jpg" width="600px" height="300px"/>
-                <div className={classes.Buttons}>
-                    <Grid container spacing={2} justify="center">
-                        <Grid item >
-                            <Button variant="contained" color="primary" onClick={handleClickOpen}>
-                                Create New
-                            </Button>
-                            <Dialog open={dialogOpen} TransitionComponent={Transition} keepMounted onClose={handleClose}
-                                    aria-labelledby="alert-dialog-slide-title" aria-describedby="alert-dialog-slide-description" >
-                                <DialogTitle id="alert-dialog-slide-title">{"Create New Formation"}</DialogTitle>
-                                <DialogContent>
-                                    <DialogContentText id="alert-dialog-slide-description">
-                                        Please write down some basic information before proceeding
-                                    </DialogContentText>
-                                    <TextField autoFocus margin="dense" id="name" label="Dance Name" type="dance name" fullWidth/>
-                                    <TextField autoFocus margin="dense" id="number" label="Number of Dancers" type="dancers number" fullWideth />
+        return(
+            <React.Fragment>
+                <Header headerTitle={"Welcome to Formation Helper"}/>
+                <div className="App" >
+                    <div>
+                        <h1>Welcome to Formation Helper</h1>
+                    </div>
+                    <div>
+                        <p>this is a text this is a text this is a text this is a text this is a text</p >
+                        <p>this is a text this is a text this is a text this is a text this is a text</p >
+                        <p>this is a text this is a text this is a text this is a text this is a text</p >
+                        <p>this is a text this is a text this is a text this is a text this is a text</p >
+                        <p>this is a text this is a text this is a text this is a text this is a text</p >
+                    </div>
+                    < img src="http://hochstein.org/portals/0/gottaSingDance%20pic%20for%20web.jpg" width="600px" height="300px"/>
+                    <div className={useStyles.Buttons}>
+                        <Grid container spacing={2} justify="center">
+                            <Grid item >
+                                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                                    Create New
+                                </Button>
+                                <Dialog open={this.state.dialogOpen} TransitionComponent={Transition} keepMounted onClose={this.handleClose}
+                                        aria-labelledby="alert-dialog-slide-title" aria-describedby="alert-dialog-slide-description" >
+                                    <DialogTitle id="alert-dialog-slide-title">{"Create New Formation"}</DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText id="alert-dialog-slide-description">
+                                            Please write down some basic information before proceeding
+                                        </DialogContentText>
+                                        <TextField autoFocus margin="dense" id="name" label="Dance Name" type="dance name" fullWidth/>
+                                        <TextField autoFocus margin="dense" id="number" label="Number of Dancers" type="dancers number" fullWideth />
 
-                                </DialogContent>
-                                <DialogActions>
-                                    <Button component={Link} to="/main">
-                                        Confirm
-                                    </Button>
-                                    <Button onClick={handleClose}>
-                                        Cancel
-                                    </Button>
-                                </DialogActions>
-                            </Dialog>
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button component={Link} to="/main">
+                                            Confirm
+                                        </Button>
+                                        <Button onClick={this.handleClose}>
+                                            Cancel
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary">
+                                    Load Local
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary">
+                                    DownLoad Online
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary" component={Link} to="/credit">
+                                    Credits
+                                </Button>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Button variant="outlined" color="primary">
-                                Load Local
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="outlined" color="primary">
-                                DownLoad Online
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="outlined" color="primary" component={Link} to="/credit">
-                                Credits
-                            </Button>
-                        </Grid>
-                    </Grid>
+                    </div>
                 </div>
-            </div>
-            <Footer/>
-        </React.Fragment>
-    )
+                <Footer/>
+            </React.Fragment>
+        )
+    }
 }
+
+export default WelcomePage;
