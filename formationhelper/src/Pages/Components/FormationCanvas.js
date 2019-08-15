1ae093562db4322f786c8e2d7e40082892394c27
paranoiark@outlook.com <paranoiark@outlook.com>
2019-08-15 11:40:18 -0400

    Updated Main Stage

--- formationhelper/src/Pages/Components/FormationCanvas.js
+++ formationhelper/src/Pages/Components/FormationCanvas.js
@@ -6,61 +6,38 @@ import Typography from '@material-ui/core/Typography';
 
 
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      padding: theme.spacing(3, 2),
-    },
-    items: {
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(2),
-    },
-  }));
-
-
 
 export default function FormationCanvas(){
-    const classes = useStyles();
 
     return (
-        <React.Fragment>
-            <CssBaseline>
-                <Container  className="Canvas" >
+        <div className="root">
+            <Paper className="Canvas" >
+                <div className="Front" >
+                    <Typography>Front</Typography>
+                </div>
 
-                    <Grid className={classes.items} container spacing={1}>
-                        <Grid item xs ={12}>
-                            <Paper className="Direction">
-                                <Typography  variant ="h5" align="center">Front</Typography>
-                            </Paper>
-                        </Grid>
+                <div className="LeftStage">
+                    <Typography>Off Stage</Typography>
+                </div>
 
-                        <Grid item xs={1}><Paper  className="Paper" >OffStage</Paper></Grid>
+                <div className="Stage">
+                    <div >
+                        <Typography variant={"h4"}> Stage </Typography>
+                        <div >
+                            <Dot color="#f7941f"/>
+                            <Dot color="#f44336"/>
+                        </div>
+                    </div>
+                </div>
 
-                        <Grid item xs={10}>
-                            <Paper className="Stage" >
-                                {/* TODO: Use Grid for now, but should be replaced with a canvas component that can specify the position*/}
-                                <Grid container direction="column" justify="center" alignItems="center">
-                                    <Typography> Stage </Typography>
-                                    <Grid item sm>
-                                        <Dot color="#000000"/>
-                                        <Dot color="#245662"/>
-                                    </Grid>
-                                </Grid>
-                            </Paper>
-                        </Grid>
+                <div className="RightStage">
+                    <Typography>Off Stage</Typography>
+                </div>
 
-                        <Grid item xs={1}>
-                            <Paper className="Paper">OffStage</Paper>
-                        </Grid>
-
-                        <Grid item xs = {12} marginTop="theme.spacing{4}">
-                            <Paper className = "Direction">
-                                <Typography  variant ="h5" align = "center">Back</Typography>
-                            </Paper>
-                        </Grid>
-
-                    </Grid>
-                </Container>
-            </CssBaseline>
-        </React.Fragment>
+                <div className = "Back" >
+                    <Typography>Back</Typography>
+                </div>
+            </Paper>
+        </div>
     );
 }
