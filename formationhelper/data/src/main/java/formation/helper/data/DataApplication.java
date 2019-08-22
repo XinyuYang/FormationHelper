8d58d2bbc1d22f9a37735f388b18197fd5b53cde
XinyuYang <xyang2@macalester.edu>
2019-08-22 18:00:43 +0800

    Modified basic spring boot backend

--- formationhelper/data/src/main/java/formation/helper/data/DataApplication.java
+++ formationhelper/data/src/main/java/formation/helper/data/DataApplication.java
@@ -1,4 +1,4 @@
-package com.formation.data;
+package formation.helper.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- formationhelper/data/src/test/java/formation/helper/data/DataApplicationTests.java
+++ formationhelper/data/src/test/java/formation/helper/data/DataApplicationTests.java
@@ -1,4 +1,4 @@
-package com.formation.data;
+package formation.helper.data;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
