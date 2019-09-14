3bdf92c4dc2bf0cc39a5d082ca91d50470b5e023
yurenpang <ypang@macalester.edu>
2019-09-14 11:28:25 -0500

    Create Empty Dancer Class.

--- formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Dancer.java
+++ formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Dancer.java
@@ -1,4 +1,16 @@
 package com.formationHelper.formationHelper;
 
+/**
+ * Dancer class will implement more functionality in the future including
+ * gender, phone #, class, photo, etc
+ *
+ */
 public class Dancer {
+    private int id;
+    private String name;
+
+    public Dancer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
