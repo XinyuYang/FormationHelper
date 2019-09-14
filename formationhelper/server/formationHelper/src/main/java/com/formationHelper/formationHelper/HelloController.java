75a562e6a7a880e5b3db7e54d7877d69d0242116
Yuxiang Chen <chenyx.eric2013@gmail.com>
2019-09-13 19:14:47 -0500

    Remove Spring Security

---
+++ formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/HelloController.java
@@ -0,0 +1,14 @@
+package com.formationHelper.formationHelper;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!!!";
+    }
+
+}
