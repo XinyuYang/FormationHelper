1b1bf6aa42f0d3f62d7a8a2a08d64de060a0f97a
yurenpang <ypang@macalester.edu>
2019-09-14 12:31:50 -0500

    Create empty dancer and its basic functionality.

--- formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Dancer.java
+++ formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Dancer.java
@@ -1,5 +1,8 @@
 package com.formationHelper.formationHelper;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Dancer class will implement more functionality in the future including
  * gender, phone #, class, photo, etc
@@ -8,9 +11,18 @@ package com.formationHelper.formationHelper;
 public class Dancer {
     private int id;
     private String name;
+    private double x, y;
 
-    public Dancer(int id, String name) {
+    public Dancer(int id, String name, double x, double y) {
         this.id = id;
         this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "; Name: " + name +
+                "; x: " + x + "; y: " + y;
     }
 }
