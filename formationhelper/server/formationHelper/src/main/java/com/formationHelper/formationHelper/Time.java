d95c831effb437d41c2a3fd5b7a63759ce464c4b
yurenpang <ypang@macalester.edu>
2019-09-14 12:32:46 -0500

    Create Time to store minute and second.

---
+++ formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Time.java
@@ -0,0 +1,19 @@
+package com.formationHelper.formationHelper;
+
+public class Time {
+    private int minute;
+    private int second;
+
+    public Time(int minute, int second) {
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+}
