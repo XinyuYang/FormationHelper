d4fca987593088e4c3d558d6f0c3c0d192fde850
Yuhan Chen <ychen3@macalester.edu>
2019-09-15 10:49:46 -0400

    update the formation canvas
    Try to solve the problem that line out of stage

--- formationhelper/src/Pages/Components/FormationCanvas.js
+++ formationhelper/src/Pages/Components/FormationCanvas.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline, Container, Paper, Grid } from '@material-ui/core';
+import {CssBaseline, Container, Paper, Grid} from '@material-ui/core';
 import Dot from "./Dot";
 import Typography from '@material-ui/core/Typography';
-import Draggable from 'react-draggable'
 
-function createArr(num){
+function createArr(num) {
     let arr = [];
-    for(let i = 0; i < num; i++) arr.push(i);
+    for (let i = 0; i < num; i++) arr.push(i);
     return arr
 }
 
 class StageSection extends Component {
     constructor(props) {
         super(props);
-        this.state = { offset: 0 }
+        this.state = {offset: 0};
     }
 
     handleClick(offset) {
@@ -23,44 +21,82 @@ class StageSection extends Component {
 
     render() {
         return (
-                <div className="Stage" style={{direction:"column", justify:"center", alignItems: "center", position: 'relative'}}>
-                    {/* TODO: Use Grid for now, but should be replaced with a canvas component that can specify the position*/}
-                    {/* Dot only works using div */}
+            <div ref={this.myRef} className="Stage"
+                 style={{direction: "column", justify: "center", alignItems: "center", position: 'relative'}}>
+                {/* TODO: Use Grid for now, but should be replaced with a canvas component that can specify the position*/}
+                {/* Dot only works using div */}
 
-                    {createArr(this.props.dancer).map((index) =>
-                        <Dot name = {'dot'+index} color = "#000000" />
-                    )}
-                </div>
+                {createArr(this.props.dancer).map((index) =>
+                    <Dot name={'dot' + index} color="#000000"/>
+                )}
+            </div>
         )
     }
 }
 
 
-export default function FormationCanvas(formationInfo){
-
-    return (
-        <div className="root">
-            <Paper className="Canvas" >
-                <div className="Front" >
-                    <Typography>Front</Typography>
-                </div>
-
-                <div className="LeftStage">
-                    <Typography>Off Stage</Typography>
-                </div>
+class FormationCanvas extends Component {
+    state = {};
+    componentDidMount = async () => {
+        let element = document.querySelector('.Canvas');
+        let style = window.getComputedStyle(element);
+        let width = style.getPropertyValue('width');
+        await this.setState({width});
+        console.log(this.state.width)
+    };
 
 
-                <StageSection dancer={formationInfo.dancerNumber}/>
+    render() {
+        return (
+            <div className="root">
+                <Paper className="Canvas">
+                    <div>
+                        {createArr(62).map((index) =>
+                            <hr className="vl" style={{
+                                position: "absolute",
+                                opacity: 0.1,
+                                color: '#000000',
+                                borderColor: '#000000',
+                                marginLeft: 110 + index * 20,
+                            }}/>
+                        )}
+                    </div>
+                    <div>
+                        {createArr(25).map((index) =>
+                            <hr className="hl" style={{
+                                position: "absolute",
+                                opacity: 0.1,
+                                color: '#000000',
+                                borderColor: '#000000',
+                                marginTop: index * 20,
+                            }}/>
+                        )}
+                    </div>
+                    <div className="Front">
+                        <Typography>Front</Typography>
+                    </div>
+
+                    <div className="LeftStage">
+                        <Typography>Off Stage</Typography>
+                    </div>
 
 
-                <div className="RightStage">
-                    <Typography>Off Stage</Typography>
-                </div>
+                    <StageSection dancer={this.props.dancerNumber}/>
 
-                <div className = "Back" >
-                    <Typography>Back</Typography>
-                </div>
-            </Paper>
-        </div>
-    );
+
+                    <div className="RightStage">
+                        <Typography>Off Stage</Typography>
+                    </div>
+
+                    <div className="Back">
+                        <Typography>Back</Typography>
+                    </div>
+                </Paper>
+            </div>
+        );
+    }
 }
+
+export default FormationCanvas;
+
+
