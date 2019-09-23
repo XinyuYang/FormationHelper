295a227a0db587823599a91894b5f49e5f72e6e2
adiawang <adiaallthewayup@sjtu.edu.cn>
2019-09-23 19:50:02 -0400

    Move addDancer(), deleteDancer(), getDancer() to Dance Class

--- formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Formation.java
+++ formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Formation.java
@@ -5,10 +5,7 @@
  */
 package com.formationHelper.formationHelper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Formation {
     private int id;
@@ -16,16 +13,15 @@ public class Formation {
     private String name;
     private int duration;  // in seconds
     private Time startTime, endTime;
-
     private List<Dancer> dancers;
-    private Map<Integer, Dancer> dancerMap;
-    private Map<Integer, Integer> idMap;
 
     /**
      * The formation has id, name, duration (minutes, seconds for a dance)
      * dancerMap, idMap are helper map to get and remove
-     *
+     * TODO:
+     *      Add default Formation for different number of people
      */
+
     public Formation(int id, String name, int numDancers, Time startTime, Time endTime) {
         this.id = id;
         this.name = name;
@@ -35,9 +31,6 @@ public class Formation {
         this.duration = (endTime.getMinute() - startTime.getMinute()) * 60 +
                 (endTime.getSecond() - startTime.getSecond());
 
-        this.dancers = new ArrayList<>();
-        this.dancerMap = new HashMap<>();
-        this.idMap = new HashMap<>();
     }
 
     /**
@@ -45,38 +38,6 @@ public class Formation {
      */
     public List<Dancer> getFormation() {
         return this.dancers;
-    }
-
-    public void addDancer(int id, String dancerName, double x, double y) {
-        if(idMap.size() > numDancers) { // check if there are more people than required
-            System.out.println("Warning: more dancers than designed");
-            return;
-        }
-
-        Dancer newDancer = new Dancer(id, dancerName, x, y);
-        dancers.add(newDancer);
-        dancerMap.put(id, newDancer);
-        idMap.put(id, dancers.size()-1);   // Go find the index in list
-    }
-
-    public void deleteDancer(int id) {
-        if(!idMap.containsKey(id)) {
-            System.out.println("Couldn't find the this dancer");
-            return;
-        }
-
-        int index = idMap.get(id);
-        dancers.remove(index);
-        dancerMap.remove(id);
-        idMap.remove(id);
-    }
-
-    public Dancer getDancer(int id) {
-        if(!idMap.containsKey(id)) {
-            System.out.println("Error: user not found!");
-            return null;
-        }
-        return dancerMap.get(id);
     }
 
     @Override
