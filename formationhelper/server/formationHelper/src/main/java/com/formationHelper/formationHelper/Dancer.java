a6dae8ac22ab5b99c7e15ff2d8362cf6adb47756
adiawang <adiaallthewayup@sjtu.edu.cn>
2019-09-23 19:48:23 -0400

    Add setLocation() to change location after initialization

--- formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Dancer.java
+++ formationhelper/server/formationHelper/src/main/java/com/formationHelper/formationHelper/Dancer.java
@@ -20,6 +20,11 @@ public class Dancer {
         this.y = y;
     }
 
+    public void setLocation(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public String toString() {
         return "ID: " + id + "; Name: " + name +
