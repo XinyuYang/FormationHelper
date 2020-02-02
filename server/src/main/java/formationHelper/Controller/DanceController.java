1ae7d66b9209403c1950db1fc565768ad47f2370
Yuxiang Chen <chenyx.eric2013@gmail.com>
2020-02-02 17:22:19 -0500

    Add usage documentation to danceController.java

--- server/src/main/java/formationHelper/Controller/DanceController.java
+++ server/src/main/java/formationHelper/Controller/DanceController.java
@@ -18,20 +18,24 @@ public class DanceController {
     @Autowired
     private UserRepository userRepository;
 
-    // Get all of the dances created by the owner of the account
+    // Function: Get all of the dances created by the owner of the account
+    // Usage: This can be used in the home/page to let users view all dances created by themselves
     @GetMapping("/user/{userId}/getAllDances")
     public @ResponseBody Page<Dance>getAllDancesFromUser(@PathVariable (value = "userId") Integer userId,
                                   Pageable pageable){
         return danceRepository.findByUserId(userId, pageable);
     }
 
-    // Added a method to get all dances from the server
-    // implement a proper search method (E.G. return all dances from server with a Pageable, sorted by alphabetical order)
+    // Function: Get all dances from the server
+    // Usage: 1. Implement a proper search method (E.G. return all dances from server with a Pageable, sorted by alphabetical order)
+    //        2. Allow users to view all dances in the server, choosing one to start building (not from scratch)
     @GetMapping("/getAllDances")
     public @ResponseBody Page<Dance>getAllDances(Pageable pageable){
         return danceRepository.findAll(pageable);
     }
 
+    // Function: create a dance and store related information into database
+    // Usage: can be used with the create button after user fill out the required information to build a formation
     @PostMapping("/user/{userId}/createDance")
     public @ResponseBody Dance addDance(@PathVariable(value = "userId") Integer userId,
                           @RequestBody Dance dance){
@@ -41,6 +45,8 @@ public class DanceController {
         }).orElseThrow(() -> new ResourceNotFoundException("UserId "+userId+" Not Found"));
     }
 
+    // Function: update the related information for a specific dance
+    // Usage: can be used for the save button that saves all the changes made in the dance
     @PutMapping("/user/{userId}/saveDance/{danceId}")
     public @ResponseBody Dance saveDance(@PathVariable (value="userId") Integer userId,
                                          @PathVariable (value="danceId") Integer danceId,
@@ -55,6 +61,9 @@ public class DanceController {
             return danceRepository.save(dance);
         }).orElseThrow(() -> new ResourceNotFoundException("DanceId "+danceId+" Not Found"));
     }
+    // Function: delete the specific dance
+    // Usage: can be used with the PutMapping that updatse/saves dance 
+    // !!  => delete the original record in database, then save it
     @DeleteMapping("/user/{userId}/deleteDance/{danceId}")
     public ResponseEntity<?> deleteDance(@PathVariable (value = "userId") Integer userId,
                                              @PathVariable (value = "danceId") Integer danceId){
